f26c263d24be80f9c8b30b49b6a10fec81ad15e9
Nimish Sharma <sharmanemo1994@gmail.com>
2019-05-08 18:09:24 -0500

    remove test file

--- src/main/scala/TestSimHash.scala
+++
@@ -1,9 +0,0 @@
-import simhash.SimHash
-
-object TestSimHash extends App with SimHash {
-  println(simHash("My name is Nimish"))
-  println()
-  println(simHash("luffy will find one piece"))
-  println()
-  println(simHash("Nimish is my name"))
-}
